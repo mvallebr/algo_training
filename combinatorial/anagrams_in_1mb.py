78162cbd8c3208118a2610e3474f24fd0c8876fa
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-06-19 17:44:30 +0100

    better description

--- combinatorial/anagrams_in_1mb.py
+++ combinatorial/anagrams_in_1mb.py
@@ -1,4 +1,5 @@
 """
 Given a text file with 1 MB and a string with 10 characters, find all anagrams of this 10 characters string on the file.
-
+The output should be the file positions where you found the occurencies, like:
+10,1039,42
 """
--- combinatorial/anagrams_in_1tb.py
+++ combinatorial/anagrams_in_1tb.py
@@ -1,4 +1,5 @@
 """
-Given a text file with 1 Tb and a string S with > 10k characters, find all anagrams of S on the file.
-
+Given a text file with 1 Tb and a string S with > 10k characters, find all anagrams of S in the file.
+The output should be the file positions where you found the occurencies, like:
+10,1039,42
 """
