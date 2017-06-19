57aded3b2dd10f058aa3ff5baa71e9303712f722
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-06-19 17:51:10 +0100

    juan study plan

--- combinatorial/anagrams_in_1tb.py
+++ combinatorial/anagrams_in_1tb.py
@@ -1,5 +1,5 @@
 """
-Given a text file with 1 Tb and a string S with > 10k characters, find all anagrams of S in the file.
+Given a text file with 1 Tb and a string S with > 10k characters, find all anagrams of S in the file. You can assume the string you're looking for fits in memory.
 The output should be the file positions where you found the occurencies, like:
 10,1039,42
 """
