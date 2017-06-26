935fc01115c5ab41e4c8c2e6d375284049fe011b
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-06-26 21:11:50 +0100

    combination vs permutation

--- combinatorial/all_combinations.py
+++ combinatorial/all_combinations.py
@@ -2,5 +2,5 @@
 Write an algorithms that, given the following characters:
 'abcdefghklmnopqrstuvwxyz0123456789'
 
-Generate all possible combinations of 5 characters strings using these characters.
+Generate all possible combinations of 5 characters strings using these characters, order doesn't matter.
 """
---
+++ combinatorial/all_sub_permutations.py
@@ -0,0 +1,6 @@
+"""
+Write an algorithms that, given the following characters:
+'abcdefghklmnopqrstuvwxyz0123456789'
+
+Generate all possible combinations of 5 characters strings using these characters, order matter.
+"""
