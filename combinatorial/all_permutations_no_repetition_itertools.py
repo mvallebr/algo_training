7e3d5c94f06d79e46987449c5964b6fc849ef6c5
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-06-26 20:30:56 +0100

    using itertools

--- combinatorial/all_permutations_no_repetition_itertools.py
+++ combinatorial/all_permutations_no_repetition_itertools.py
@@ -9,13 +9,6 @@ ref. https://brilliant.org/wiki/permutations-with-repetition/
 import itertools
 
 
-def count_num_perms(input):
-    c = 0
-    for x in itertools.permutations(input):
-        c += 1
-    print("Found {} permutations".format(c))
-
-
 def print_all_perms(input):
     print("\nPrinting all permutations for {}".format(input))
     for i, x in enumerate(itertools.permutations(input)):
@@ -23,4 +16,3 @@ def print_all_perms(input):
 
 
 print_all_perms(list("123"))
-# count_num_perms(list('abcdefghklmnopqrstuvwxyz0123456789'))
