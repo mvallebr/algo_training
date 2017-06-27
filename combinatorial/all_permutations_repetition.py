97efe0a6e308db2f7c75e1a00761610c3e8abb2e
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-06-27 19:37:55 +0100

    better problem description

--- combinatorial/all_permutations_repetition.py
+++ combinatorial/all_permutations_repetition.py
@@ -1,8 +1,9 @@
 """
-Write an algorithms that generates all permutations of the string 
-'abcdefghklmnopqrstuvwxyz00112233445566778899'
+Write an algorithms that generates all permutations of strings with repeated chars, like 
+'11234' or '11111' or '11211'
 
 Realize that order is not important when character is the same.
 
 ref. https://betterexplained.com/articles/easy-permutations-and-combinations/
 """
+
