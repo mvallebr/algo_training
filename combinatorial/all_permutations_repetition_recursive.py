e20ad0e5b0bfb1061a7c633dff6dd470d6b13273
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-06-30 20:54:53 +0100

    found algorithm L

--- combinatorial/all_permutations_repetition_recursive.py
+++ combinatorial/all_permutations_repetition_recursive.py
@@ -8,20 +8,24 @@ ref. https://betterexplained.com/articles/easy-permutations-and-combinations/
 """
 
 
+# SOLUTION base on algorithm L - Knuth
+# Renzo implemented a nice solution based on dictionary comparison - https://github.com/renzon/code_interview_training/blob/0e06e1d872ebc8edf1a2667296f95e15b9172378/perm_with_rep.py#L19-L19
+
 def find_all_permutations_r(prefix, sublist):
     if len(sublist) == 1:
+        print ("prefix = {} sublist = {}".format(prefix, sublist))
         yield prefix + sublist
+        return
     for pivot in range(len(sublist)):
-        print ("pivot = {} {} {}".format(pivot, sublist[pivot], sublist[0]))
+        print ("prefix = {} sublist = {} pivot = {}".format(prefix, sublist, pivot))
         # Exchange first char by pivot
-        if sublist[pivot] == sublist[0]:
-            yield prefix + sublist
+        if (sublist[0] == sublist[pivot] and pivot != 0) or sublist[0] in prefix:
+            print ("continue")
             continue
         temp = sublist[pivot]
         sublist[pivot] = sublist[0]
         sublist[0] = temp
-        for perm in find_all_permutations_r(prefix + [sublist[0]], sublist[1:]):
-            yield perm
+        yield from find_all_permutations_r(prefix + [sublist[0]], sublist[1:])
         # Exchange back
         sublist[0] = sublist[pivot]
         sublist[pivot] = temp
@@ -38,6 +42,7 @@ def test_it(input):
         print ("p[{}] = {}".format(i+1, p))
 
 
+test_it("112")
 test_it("1112")
-#test_it("1111")
-# test_it("1234")
+test_it("1111")
+test_it("1122")
