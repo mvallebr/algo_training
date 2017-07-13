0083af632c99dc5b2c3b47c664c98e3ef14ffc67
Marcelo Valle Militao <mvallemilita@bloomberg.net>
2017-07-13 14:03:47 +0100

    sub set sum problem

---
+++ dynamic_prog/sub_set_sum.py
@@ -0,0 +1,8 @@
+"""
+Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
+
+Examples: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output:  True  //There is a subset (4, 5) with sum 9.
+"""
+
+# Solution described on http://www.geeksforgeeks.org/dynamic-programming-subset-sum-problem/
---
+++ temp/pair_of_elements_with_sum_x.py
@@ -0,0 +1,4 @@
+"""
+Given an integer X and a list L, find the first pair in L whose sum is X
+"""
+
